33638726bbd2c9cb4fa5e9eb8413d0bb9f833a15
Francisco Molina <119901915+GhostlyCoder20@users.noreply.github.com>
2024-10-15 17:45:35 -0600

    migraciones de departamentos y municipios

---
+++ database/migrations/2024_10_15_234329_create_municipalities.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('municipalities', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 45);
+            $table->foreignId('departments_id')->constrained('departments');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('municipalities', function (Blueprint $table) {
+            //
+        });
+    }
+};
