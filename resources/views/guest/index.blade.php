c0a220815500412943ff71b7f03e8dc9e3b48d51
MarlieRamirez <marlie.lucia@hotmail.com>
2024-11-01 12:55:55 -0600

    Input number

--- resources/views/guest/index.blade.php
+++ resources/views/guest/index.blade.php
@@ -20,6 +20,15 @@
         border-color: #ffac45;
     }
 
+    input::-webkit-outer-spin-button,
+    input::-webkit-inner-spin-button{
+        -webkit-appearance: none;
+        margin: 0;
+    }
+
+    input[type=number]{
+        -moz-appearance: textfield;
+    }
 </style>
 
     <section class="mascotas">
